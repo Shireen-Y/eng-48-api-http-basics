58908c22dd634753e5fac44706eb8f2fd3754198
Shireen Yawer <SYawer@spartaglobal.com>
2019-12-13 09:08:22 +0000

    .txt file created

--- new_postcode.py
+++ new_postcode.py
@@ -5,14 +5,20 @@ postcode_input = input('What is your postcode? ')
 postcode = requests.get(path_url + postcode_input)
 dictionary_response = postcode.json()
 
-class New_postcode():
-    def __init__(self, postcode):
-        self.postcode = postcode
+ask_file = input('Please name the file you would like to insert your postcode data into: ')
+file_name = ask_file + '.txt'
 
-    def get_latitude(self, file_name):
-        try:
-            with open(file_name, 'w+') as file_create:
-                file_create.write(f"Your latitude is: {dictionary_response['result']['latitude']}")
-        finally:
-            print('')
+try:
+    with open(file_name, 'w+') as file_created:
+        file_created.write(f"Postcode: {dictionary_response['result']['postcode']}" + "\n")
+        file_created.write(f"Longitude: {dictionary_response['result']['longitude']}" + "\n")
+        file_created.write(f"Latitude: {dictionary_response['result']['latitude']}" + "\n")
+        file_created.write(f"Nut: {dictionary_response['result']['nuts']}" + "\n")
+        file_created.write(f"Admin ward: {dictionary_response['result']['admin_ward']}" + "\n")
+
+except TypeError as error:
+    print('Please ensure you have all the correct information')
+
+finally:
+    print('All complete')
 
